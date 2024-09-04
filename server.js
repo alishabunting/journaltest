4dce483a179c2249cfc7041df63c7cdd3255ff95
unknown <alisha.bunting1@gmail.com>
2024-09-04 15:14:09 -0400

    server js errors fixed

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 app.use(express.static('.'));
-app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
